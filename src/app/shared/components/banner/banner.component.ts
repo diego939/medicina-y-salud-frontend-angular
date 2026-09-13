eafbf9472ea49f4dcf0fe98aab04065b4b562ad9
diego939 <diegodavidalmiron1990@gmail.com>
2026-09-13 18:50:48 -0300

    fix add loading lazy in banner

--- src/app/shared/components/banner/banner.component.ts
+++ src/app/shared/components/banner/banner.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  PLATFORM_ID
+} from '@angular/core';
+
 import { isPlatformBrowser } from '@angular/common';
 import { initFlowbite } from 'flowbite';
 
@@ -10,11 +16,45 @@ import { initFlowbite } from 'flowbite';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements AfterViewInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       initFlowbite();
     }
+  }
+
+  onImageLoad(event: Event): void {
+    const image = event.target as HTMLImageElement;
+
+    const loader = image.parentElement?.querySelector(
+      '.image-loader'
+    ) as HTMLElement | null;
+
+    image.classList.remove('opacity-0');
+    image.classList.add('opacity-100');
+
+    if (loader) {
+      loader.classList.add('opacity-0');
+
+      setTimeout(() => {
+        loader.remove();
+      }, 500);
+    }
+  }
+
+  onImageError(event: Event): void {
+    const image = event.target as HTMLImageElement;
+
+    const loader = image.parentElement?.querySelector(
+      '.image-loader'
+    ) as HTMLElement | null;
+
+    loader?.remove();
+
+    image.classList.remove('opacity-0');
   }
 }
